00cfba80094a021e6156707fddf541be87ce5114
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-05-01 12:35:03 +0200

    Redirect if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
+import { IsUserRedirect } from './helpers/routes';
 
 import * as ROUTES from './constants/routes';
 
 export default function App() {
+  const user = {};
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
+      <Route path={ROUTES.SIGN_UP}>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact />
         <Signup />
       </Route>
       <Route exact path={ROUTES.SIGN_IN}>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact />
         <Signin />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
       </Route>
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
