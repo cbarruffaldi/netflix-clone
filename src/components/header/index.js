a947ff79219de310b206fe5507ee86e40317b081
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-05-21 16:03:26 +0200

    Header Play button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -16,6 +16,7 @@ import {
   Search,
   SearchIcon,
   SearchInput,
+  PlayButton,
 } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -86,4 +87,8 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
       />
     </Search>
   );
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
